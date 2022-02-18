9de25f5548741676aef3c9380c6bf83baff6cd4a
IDoston <dostoninogomov3@gmail.com>
2022-02-18 21:38:12 +0300

    prompt

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var isName = prompt("ismingizni kiriting","Doston") ;
+var isBootstap = prompt("bootstrap nima va nima uchun kerak") ;
+var isCarousel = prompt("bootstrap da Carousel nima uchun kerak ") ;
+var isAlert = prompt("bootstrap da alert nima uchun kerak va qanday yoziladi") ;
+var isInput = prompt("bootstrap da input classi qanday yoziladi") ;
+var isBtn = prompt("bootstrap da textga ranga berish uchun qaysi classdan Foydalaniladi") ;
+var isJavascript = prompt("Java scriptda var nima uchun kerak") ;
+
+console.log(
+    isName + "\n" +isBootstap + "\n" +isCarousel +
+"\n" +  isAlert + "\n" + isInput + "\n" + isBtn + "\n" + isJavascript )
